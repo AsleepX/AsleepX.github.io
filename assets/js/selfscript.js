e5bfcefae70d41e125875111f2ad3e0eb3c4f99c
AsleepX <129823735+AsleepX@users.noreply.github.com>
2023-10-25 19:44:27 +0800

    Create selfscript.js

---
+++ assets/js/selfscript.js
@@ -0,0 +1,14 @@
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.anime-image');
+
+function changeImage() {
+    images[currentImageIndex].style.display = 'none'; // 隐藏当前图片
+    currentImageIndex = (currentImageIndex + 1) % images.length; // 计算下一个图片的索引
+    images[currentImageIndex].style.display = 'block'; // 显示下一个图片
+}
+
+// 初始显示第一张图片
+images[0].style.display = 'block';
+
+// 设置循环切换图片的间隔时间
+setInterval(changeImage, 3000); // 每3秒切换一次图片
